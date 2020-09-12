f6d0cf2a8697301ecfe2ac2a6822c9726089f61e
German <german.qf33@gmail.com>
2020-09-12 16:15:03 -0500

    config Mongo URL

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if(process.env.NODE_ENV ==='dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://geraqf:Cm1OKvz6TkazXkMj@cluster0.afeyl.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 
